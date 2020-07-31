1498c96769ac4ab70936823e6cc9534807b62eac
jacekv <jaca347@gmail.com>
2020-07-31 21:08:21 +0200

    Extend registry contract

--- truffle/contracts/IconRegistry.sol
+++ truffle/contracts/IconRegistry.sol
@@ -2,7 +2,14 @@ pragma solidity 0.6.0;
 
 contract IconRegistry {
   address public owner;
-  mapping (address=>string) public registry;
+  mapping (address=>SmartContractInformation) public registry;
+  address[] public scAddresses;
+
+  struct SmartContractInformation {
+    string _name;
+    string _path;
+    string _type;
+  }
 
   constructor() public {
     owner = msg.sender;
@@ -10,13 +17,27 @@ contract IconRegistry {
 
   /**
    * _target - Address of smart contract the icon is for
-   * _value  - Path to the icon
+   * _path   - Path to the icon
+   * _name   - Name of the smart contract
+   * _type   - Type of the smart contract (ERC20, etc.)
    */
-  function setRegistryValue(address _target, string memory _value) public {
+  function setRegistryValue(
+    address _target,
+    string memory _path,
+    string memory _name,
+    string memory _type) public {
       Owner c = Owner(_target);
       // check if the sender of the transaction is the owner of the smart contract
       require(c.owner() == msg.sender, 'You are not the owner of the contract');
-      registry[_target] = _value;
+      registry[_target] = SmartContractInformation(_name, _path, _type);
+      scAddresses.push(_target);
+  }
+
+  /**
+   * Returns array which contains all registered smart contract addresses
+   */
+  function getSCAddresses() public view returns (address[] memory) {
+      return scAddresses;
   }
 }
 
