af5fc5ed1566ff0a6d568eae7501484cf1f9eea8
jacekv <jaca347@gmail.com>
2020-07-31 21:08:48 +0200

    Add test for adding 1st smart contract to registry

--- truffle/test/IconRegistry.test.js
+++ truffle/test/IconRegistry.test.js
@@ -1,10 +1,37 @@
 const IconRegistry = artifacts.require("IconRegistry");
+const OwnerTestContract = artifacts.require("OwnerTestContract");
 
-contract("IconRegistry Test", async accounts => {
-  it("set an address value", async () => {
-    let instance = await IconRegistry.deployed();
-    await instance.setRegistryValue(accounts[0], "lalala");
-    let value = await instance.registry(accounts[0]);
-    assert.equal(value, "lalala");
+contract("IconRegistry Test", async () => {
+  let IconRegistryInstance;
+  let OwnerTestContractInstance;
+
+  before(async () => {
+    IconRegistryInstance = await IconRegistry.deployed();
+    OwnerTestContractInstance = await OwnerTestContract.deployed();
+  });
+
+  it("register first smart contract", async () => {
+    await IconRegistryInstance.setRegistryValue(
+      OwnerTestContractInstance.address,
+      "lalala",
+      "AwesomeContract",
+      "ERC20"
+    );
+    let entry = await IconRegistryInstance.registry(
+      OwnerTestContractInstance.address
+    );
+    assert.equal(entry["_path"], "lalala");
+    assert.equal(entry["_name"], "AwesomeContract");
+    assert.equal(entry["_type"], "ERC20");
+  });
+
+  it("length of array should be 1", async () => {
+    let scAddresses = await IconRegistryInstance.getSCAddresses();
+    assert.equal(scAddresses.length, 1);
+  });
+
+  it("check address of first smart contract in registry", async () => {
+    let scAddresses = await IconRegistryInstance.getSCAddresses();
+    assert.equal(scAddresses[0], OwnerTestContractInstance.address);
   });
 });
