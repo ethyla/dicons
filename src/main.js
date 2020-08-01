b4bee95cc6cd43297a663f51e8dc6f5d76bf588d
jacekv <jaca347@gmail.com>
2020-08-01 22:02:49 +0200

    Adding material design icons

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import "@babel/polyfill";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
-import "@mdi/font/css/materialdesignicons.css";
 
 Vue.config.productionTip = false;
 
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 
 Vue.use(Vuetify);
 
@@ -17,5 +18,8 @@ export default new Vuetify({
         warning: "#FFC107"
       }
     }
+  },
+  icons: {
+    iconfont: "md" // default - only for display purposes
   }
 });
